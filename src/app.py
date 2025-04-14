5f61ed131fd6588c8083efd9807359ebedf9c683
annisanurfadilah018@gmail.com <annisanurfadilah018@gmail.com>
2025-04-14 22:51:27 +0800

    Update path

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 # Load model (gunakan path relatif untuk deployment)
-model = tf.keras.models.load_model('model_mental_health_v1.keras')
+model = tf.keras.models.load_model('model_mental_health_v1.h5')
 
 # Inisialisasi Tokenizer (harus sama dengan saat training)
 tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
